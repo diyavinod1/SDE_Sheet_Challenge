a40bf6991861373a6a8d6f554aebcb42a5602bf7
Diya Vinod <dhiyavinodarrived@gmail.com>
2026-06-13 16:21:31 +0530

    Create q2_clone_a_ll_with_random_and_next_pointer.cpp

---
+++ Day13/q2_clone_a_ll_with_random_and_next_pointer.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if (!head) return NULL;
+
+        Node* curr = head;
+
+        while (curr) {
+            Node* copy = new Node(curr->val);
+
+            copy->next = curr->next;
+            curr->next = copy;
+
+            curr = copy->next;
+        }
+
+        curr = head;
+
+        while (curr) {
+            if (curr->random) {
+                curr->next->random = curr->random->next;
+            }
+
+            curr = curr->next->next;
+        }
+
+        Node* dummy = new Node(0);
+        Node* copyCurr = dummy;
+
+        curr = head;
+
+        while (curr) {
+            Node* copy = curr->next;
+
+            copyCurr->next = copy;
+            copyCurr = copy;
+
+            curr->next = copy->next;
+            curr = curr->next;
+        }
+
+        return dummy->next;
+    }
+};
