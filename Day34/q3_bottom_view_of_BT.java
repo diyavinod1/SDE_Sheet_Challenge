696a0b267b46f28e6fc99d2a984416042b22e12a
Diya Vinod <dhiyavinodarrived@gmail.com>
2026-07-04 21:15:20 +0530

    Create q3_bottom_view_of_BT.java

---
+++ Day34/q3_bottom_view_of_BT.java
@@ -0,0 +1,63 @@
+/*********************************************
+
+ Following is the TreeNode class structure
+
+ class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+    
+     TreeNode(int val) {
+         this.val = val;
+         this.left = null;
+         this.right = null;
+     }
+ }
+
+ *********************************************/
+
+import java.util.*;
+
+public class Solution {
+
+    static class Pair {
+        TreeNode node;
+        int hd;
+
+        Pair(TreeNode node, int hd) {
+            this.node = node;
+            this.hd = hd;
+        }
+    }
+
+    public static List<Integer> bottomView(TreeNode root) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        if (root == null)
+            return ans;
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        Queue<Pair> q = new LinkedList<>();
+
+        q.offer(new Pair(root, 0));
+
+        while (!q.isEmpty()) {
+
+            Pair curr = q.poll();
+
+            map.put(curr.hd, curr.node.val);
+
+            if (curr.node.left != null)
+                q.offer(new Pair(curr.node.left, curr.hd - 1));
+
+            if (curr.node.right != null)
+                q.offer(new Pair(curr.node.right, curr.hd + 1));
+        }
+
+        for (int val : map.values())
+            ans.add(val);
+
+        return ans;
+    }
+}
