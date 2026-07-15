c9498be5718237831ff67a34de8516bfc976c934
Diya Vinod <dhiyavinodarrived@gmail.com>
2026-07-15 17:59:08 +0530

    Update q2_K-th_largest_element_in_an_unsorted_array.java

--- Day45/q2_K-th_largest_element_in_an_unsorted_array.java
+++ Day45/q2_K-th_largest_element_in_an_unsorted_array.java
@@ -1,47 +1,16 @@
 class Solution {
-
     public int findKthLargest(int[] nums, int k) {
-        int target = nums.length - k;
-        return quickSelect(nums, 0, nums.length - 1, target);
-    }
 
-    private int quickSelect(int[] nums, int low, int high, int target) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        int pivot = partition(nums, low, high);
+        for (int num : nums) {
 
-        if (pivot == target)
-            return nums[pivot];
+            pq.offer(num);
 
-        if (pivot < target)
-            return quickSelect(nums, pivot + 1, high, target);
-
-        return quickSelect(nums, low, pivot - 1, target);
-    }
-
-    private int partition(int[] nums, int low, int high) {
-
-        int pivot = nums[high];
-
-        int i = low;
-
-        for (int j = low; j < high; j++) {
-
-            if (nums[j] <= pivot) {
-
-                swap(nums, i, j);
-                i++;
-            }
+            if (pq.size() > k)
+                pq.poll();
         }
 
-        swap(nums, i, high);
-
-        return i;
-    }
-
-    private void swap(int[] nums, int i, int j) {
-
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+        return pq.peek();
     }
 }
